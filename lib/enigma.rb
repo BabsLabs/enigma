51dba53077476af819fadda4ed10b442e5f25d28
Brian Bower <brian@brianbower.com>
2019-09-14 12:15:41 -0600

    Add framework for encrypt method encryption_hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,6 +4,11 @@ class Enigma
   end
 
   def encrypt(message, key = RandomNumberGenerator.generate_random_key, date = Date.generate_todays_date)
+    encryption_hash = Hash.new
+    encryption_hash[:encryption]  = message # Needs changed to encrypted message
+    encryption_hash[:key] = key
+    encryption_hash[:date] = date
+    encryption_hash
   end
 
 end
