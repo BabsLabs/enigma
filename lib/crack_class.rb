335786c3fcf61969312e038f04b2d8954e573841
Brian Bower <brian@brianbower.com>
2019-09-17 20:15:27 -0600

    Add command line interface for crack

---
+++ lib/crack_class.rb
@@ -0,0 +1,18 @@
+require_relative 'decryptionable'
+require './lib/date'
+
+class Crack
+  include Decryptionable
+
+  def crack_helper(cyphertext, date)
+    crack_hash = Hash.new
+    cyphertext_copy = cyphertext
+    until cyphertext_copy[-4..-1] == " end"
+      crack_hash = decryption_helper(cyphertext, key = RandomNumberGenerator.generate_random_key, date)
+      cyphertext_copy = crack_hash[:decryption]
+      # p "#{cyphertext_copy}, #{key}, #{date}" # uncomment this line to watch the crack in progress!!!
+    end
+    crack_hash
+  end
+
+end
